2daf7b00fe35fe9b82b98172c28740df970cca71
Icaa <92012266+ZahrunNafisah05@users.noreply.github.com>
2025-12-26 21:31:28 +0700

    final commit

--- src/test/java/com/demoblaze/tests/BaseTest.java
+++ src/test/java/com/demoblaze/tests/BaseTest.java
@@ -2,27 +2,37 @@ package com.demoblaze.tests;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
-import com.demoblaze.utils.ExtentReportManager;
 import com.demoblaze.pages.BasePage;
+import com.demoblaze.utils.ExtentReportManager;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 
 public class BaseTest {
 
+    protected static ExtentReports extent; // satu instance untuk semua test class
     protected WebDriver driver;
-    protected ExtentReports extent;
     protected ExtentTest test;
+
+    @BeforeAll
+    static void setupAll() {
+        extent = ExtentReportManager.getInstance();
+    }
 
     @BeforeEach
     void setup() {
         driver = BasePage.openBrowser();
-        extent = ExtentReportManager.getInstance();
     }
 
     @AfterEach
     void tearDown() {
-        extent.flush();
         driver.quit();
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        extent.flush(); // flush sekali di akhir semua test
     }
 }
